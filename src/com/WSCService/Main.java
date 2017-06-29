09aa47a548b4ec33ba93e6e8fecd7365637fd02b
Andrew Lee <andrew.james.lee88@gmail.com>
2017-06-29 19:26:19 -0400

    redo with partner.jar

--- src/com/WSCService/Main.java
+++ src/com/WSCService/Main.java
@@ -1,42 +1,45 @@
 package com.WSCService;
 
 /**
- * Created by andrewlee on 6/26/17.
+ * Created by alee on 6/29/17.
  */
-import com.sforce.soap.enterprise.*;
-import com.sforce.soap.enterprise.sobject.*;
+import com.sforce.soap.partner.*;
+import com.sforce.soap.partner.sobject.*;
 import com.sforce.ws.*;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        EnterpriseConnection connection = null;
-        Config config = new Config();
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ConnectorConfig config = new ConnectorConfig();
+        config.setUsername(Config.getUsername());
+        config.setPassword(Config.getPassword());
+
+        PartnerConnection connection = null;
         try {
-            connection = Connector.newConnection(config.getUsername(), config.getPassword());
+            connection = Connector.newConnection(config);
 
-            // create a new account
-            System.out.println("Creating a new Account...");
-            Account account = new Account();
-            account.setName("ACME Account 1");
-            SaveResult[] results = connection.create(new Account[] { account });
-            System.out.println("Created Account: " + results[0].getId());
-
-            // query for the 5 newest accounts
-            System.out.println("Querying for the 5 newest Accounts...");
+            System.out.println("Querying for the Accounts...");
             QueryResult queryResults = connection.query("SELECT Id, Name from Account " +
-                    "ORDER BY CreatedDate DESC LIMIT 5");
-            //if (queryResults.getSize() > 0) {
-            //    for (SObject a: queryResults.getRecords()) {
-            //        System.out.println("Id: " + a.getId() + " - Name: "+a.getName());
-            //    }
-            //}
+                    "ORDER BY CreatedDate DESC");
+            boolean done = false;
+            if (queryResults.getSize() > 0) {
+                while (!done) {
+                    for (SObject s : queryResults.getRecords()) {
+                        System.out.println("Id: " + s.getField("Id") + " - Name: " + s.getField("Name"));
+                    }
 
-        } catch (ConnectionException e) {
-            // TODO Auto-generated catch block
+                    if (queryResults.isDone()) {
+                        done = true;
+                    } else {
+                        queryResults = connection.queryMore(queryResults.getQueryLocator());
+                    }
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
